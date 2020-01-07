0afb92aedda8e7fa2474142645947c991f6e1cdb
Norayr Chilingarian <norayr@fqdn>
2020-01-07 18:36:41 +0400

    osawa

---
+++ lens/osawa_28mm_f2.8.sh
@@ -0,0 +1,12 @@
+
+  #exif
+  maxApertureValue="2.8"
+  focalLengthIn35mmFormat="28 mm"
+  lens="EF 1:2.8 f=28mm"
+  lensModel="${lens}"
+  lensSerial="NO. 1629808"
+  #xmp
+
+  lens="MC EF 1:2.8 f=28mm"
+  lensManufacturer="Osawa"
+
--- main.sh
+++ main.sh
@@ -1,10 +1,10 @@
  set -x
  # imports
- #source camera/mamiya_zm.sh
- source camera/olympus_xa2.sh
+ source camera/mamiya_zm.sh
+ #source camera/olympus_xa2.sh
  #source camera/porst.sh
  #source camera/mamiya_1000dtl.sh
-# source camera/lomo_instant_magellan.sh
+ #source camera/lomo_instant_magellan.sh
  #source camera/lomo_instant_wide.sh
  #source camera/mamiya_ze-2.sh
  #source camera/nikon_l35_ad.sh
@@ -12,18 +12,18 @@
  #source camera/polaroid_sx-70.sh
  #source lens/AUTO_REVUENON_MCF_50mm_F1.4.sh
  #source lens/mamiya-sekor_28mm_f3.5.sh
- source lens/d.zuiko_35mm_f3.5.sh
+ #source lens/d.zuiko_35mm_f3.5.sh
  #source lens/fuji_EBC_fujinon_SW_28mm_f3.5.sh
  #source lens/Carl_Zeiss_Jena_Flektogon_20mm_f2.8.sh
-# source lens/lomo_instant_magellan.sh
+ #source lens/lomo_instant_magellan.sh
  #source lens/lomo_instant_wide.sh
  #source lens/mamiya_rb67_50mm.sh
  #source lens/polaroid_sx-70.sh
  #source lens/nikon_l35_ad.sh
  #source film/fujicolor_c200.sh
- #source film/fujicolor_pro400h.sh
+ source film/fujicolor_pro400h.sh
  #source film/fujifilm_superia800.sh
-# source film/instax_mini.sh
+ #source film/instax_mini.sh
  #source film/instax_wide.sh
  #source film/fomapan_400.sh
  #source film/FUJICHROME_Velvia_50.sh
@@ -31,12 +31,12 @@
  #source film/polaroid_originals.sh
  #source film/fujicolor_400industrial.sh
  #source film/Lomography_Berlin_400.sh
- source film/kodak_trix.sh
+ #source film/kodak_trix.sh
  #source film/kodak_3200.sh
  #source film/adox_chm400.sh
  source flash/default.sh
  #source scanner/minolta_scan_dual_iv.sh
-# source scanner/canon_9000.sh
+ #source scanner/canon_9000.sh
  source scanner/pie_xa.sh
  source picture/default.sh
  #source location/default.sh
