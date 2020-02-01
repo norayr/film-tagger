2c0b08951dc31ca4e1890286e8474ea45e0d5283
Norayr Chilingarian <norayr@arnet.am>
2020-02-02 02:23:38 +0400

    ektar

---
+++ film/kodak_ektar_100.sh
@@ -0,0 +1,9 @@
+
+  #exif
+  iso="100"
+  filmType="135"
+  filmMaker="Kodak"
+  film="KODAK EKTAR 100"
+  filmAlias="EKTAR 100"
+  developProcess="C41"
+  rollId=""
--- main.sh
+++ main.sh
@@ -3,13 +3,13 @@
  #source camera/mamiya_zm.sh
  #sonas zm:
  #source camera/mamiya_zm_.sh
- source camera/olympus_xa2.sh
+ #source camera/olympus_xa2.sh
  #source camera/porst.sh
  #source camera/mamiya_1000dtl.sh
  #source camera/lomo_instant_magellan.sh
  #source camera/lomo_instant_wide.sh
  #source camera/mamiya_ze-2.sh
- #source camera/nikon_l35_ad.sh
+ source camera/nikon_l35_ad.sh
  #source camera/nikon_n2000.sh
  #source camera/nikon_n2000_.sh
  #source camera/mamiya_rb67.sh
@@ -22,17 +22,18 @@
  #source lens/osawa_for_nikon_28mm_f2.8_.sh
  #source lens/nikon_50mm_f1.8.sh
  #source lens/spiratone_20mm_f2.8.sh
- source lens/d.zuiko_35mm_f3.5.sh
+ #source lens/d.zuiko_35mm_f3.5.sh
  #source lens/fuji_EBC_fujinon_SW_28mm_f3.5.sh
  #source lens/Carl_Zeiss_Jena_Flektogon_20mm_f2.8.sh
  #source lens/lomo_instant_magellan.sh
  #source lens/lomo_instant_wide.sh
  #source lens/mamiya_rb67_50mm.sh
  #source lens/polaroid_sx-70.sh
- #source lens/nikon_l35_ad.sh
+ source lens/nikon_l35_ad.sh
  #source film/fujicolor_c200.sh
  #source film/fujicolor_pro400h.sh
  #source film/kodak_portra_800.sh
+ source film/kodak_ektar_100.sh
  #source film/kodak_ektar_125.sh
  #source film/kodak_ektar_25.sh
  #source film/fujifilm_superia800.sh
@@ -48,7 +49,7 @@
  #source film/kodak_5222.sh
  #source film/kodak_3200.sh
  #source film/ilford_hp5.sh
- source film/ilford_delta_400.sh
+ #source film/ilford_delta_400.sh
  #source film/adox_chm400.sh
  source flash/default.sh
  #source scanner/minolta_scan_dual_iv.sh
