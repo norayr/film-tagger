3bca9b60d8765de9a80d55027df94ff0d4d2ca76
Norayr Chilingarian <norayr@fqdn>
2020-03-23 21:06:02 +0400

    lomo orange teal

---
+++ camera/lomo_instant_orange_teal.sh
@@ -0,0 +1,8 @@
+ 
+#exif
+cameraMaker="Lomographic Society International"
+cameraModel="Lomo'Instant Automat Orange Teal (Limited Edition)"
+
+#xmp
+  cameraSerialNumber=""
+
--- main.sh
+++ main.sh
@@ -3,10 +3,11 @@
  #source camera/mamiya_zm.sh
  #sonas zm:
  #source camera/mamiya_zm_.sh
- source camera/olympus_xa2.sh
+ #source camera/olympus_xa2.sh
  #source camera/porst.sh
  #source camera/mamiya_1000dtl.sh
  #source camera/lomo_instant_magellan.sh
+ source camera/lomo_instant_orange_teal.sh
  #source camera/lomo_instant_wide.sh
  #source camera/mamiya_ze-2.sh
  #source camera/nikon_l35_ad.sh
@@ -22,7 +23,7 @@
  #source lens/osawa_for_nikon_28mm_f2.8_.sh
  #source lens/nikon_50mm_f1.8.sh
  #source lens/spiratone_20mm_f2.8.sh
- source lens/d.zuiko_35mm_f3.5.sh
+ #source lens/d.zuiko_35mm_f3.5.sh
  #source lens/fuji_EBC_fujinon_SW_28mm_f3.5.sh
  #source lens/Carl_Zeiss_Jena_Flektogon_20mm_f2.8.sh
  #source lens/lomo_instant_magellan.sh
@@ -32,6 +33,7 @@
  #source lens/nikon_l35_ad.sh
  #source film/agfa_agfacolor_100.sh
  #source film/fujicolor_c200.sh
+ #source film/fujifilm_eterna_250d.sh
  #source film/fujicolor_pro400h.sh
  #source film/fujichrome_400.sh
  #source film/kodak_portra_400.sh
@@ -40,7 +42,7 @@
  #source film/kodak_ektar_125.sh
  #source film/kodak_ektar_25.sh
  #source film/fujifilm_superia800.sh
- #source film/instax_mini.sh
+ source film/instax_mini.sh
  #source film/instax_wide.sh
  #source film/fomapan_400.sh
  #source film/FUJICHROME_Velvia_50.sh
@@ -58,8 +60,8 @@
  #source film/adox_chm400.sh
  source flash/default.sh
  #source scanner/minolta_scan_dual_iv.sh
- #source scanner/canon_9000.sh
- source scanner/pie_xa.sh
+ source scanner/canon_9000.sh
+ #source scanner/pie_xa.sh
  source picture/default.sh
  #source location/default.sh
  #source location/frozen_sevan_velvia.sh
