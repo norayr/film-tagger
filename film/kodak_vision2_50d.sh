ac9d9b2344c5810fe13177754c6e921f328f510d
Norayr Chilingarian <norayr@arnet.am>
2020-04-01 16:35:24 +0400

    kodak motion picture films

---
+++ film/kodak_vision2_50d.sh
@@ -0,0 +1,9 @@
+
+  #exif
+  iso="500"
+  filmType="135"
+  filmMaker="Kodak"
+  film="KODAK VISION 2 50D"
+  filmAlias="KODAK VISION 2 50D"
+  developProcess="C41"
+  rollId=""
---
+++ film/kodak_vision3_500t.sh
@@ -0,0 +1,9 @@
+
+  #exif
+  iso="500"
+  filmType="135"
+  filmMaker="Kodak"
+  film="KODAK VISION 3 500t 5219"
+  filmAlias="KODAK VISION 3 500t 5219"
+  developProcess="C41"
+  rollId=""
