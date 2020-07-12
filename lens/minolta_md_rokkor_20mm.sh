14da78603f69d1fb093cb3f372be31b349c2d1f7
Norayr Chilingarian <norayr@fqdn>
2020-07-12 17:47:19 +0400

    lens

---
+++ lens/minolta_md_rokkor_20mm.sh
@@ -0,0 +1,11 @@
+
+  #exif
+  maxApertureValue="2.8"
+  focalLengthIn28mmFormat="20 mm"
+  lens="MD W.ROKKOR-X 1:2.8 f=20mm"
+  lensModel="${lens}"
+  lensSerial="1202764"
+  #xmp
+
+  lensManufacturer="Minolta Co., Ltd."
+
